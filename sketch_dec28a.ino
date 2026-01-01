6c929b788356d7b0e1c8e811cf543bce2c0bc0ed
David Cabrera <127139045+SilkyDaveCodes@users.noreply.github.com>
2026-01-01 16:32:51 -0500

    Add files via upload

---
+++ sketch_dec28a.ino
@@ -0,0 +1,35 @@
+// Code from Aviyan's Thoughts
+// https://github.com/AviyansThoughts/NodeMCU-with-Ultrasonic-Sensor/blob/main/UltrasonicRange_Sensor.ino
+
+#define echoPin D5 // attach pin GPIO-14 of NodeMCU to pin Echo of HC-SR04
+#define trigPin D6 //attach pin GPIO-12 of NodeMCU to pin Trig of HC-SR04
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600);  // Serial Communication is starting with 115200 of baudrate speed
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  Serial.println("with Aries V2.0");
+}
+void loop() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(100);
+}
